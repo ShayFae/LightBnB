b53e8dc12f60244d168523135b689061a37ba0d3
ShayFae <ifrahnoor108@gmail.com>
2022-02-10 16:14:10 -0500

    user can now see their reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -85,22 +85,28 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
-
 // const getAllReservations = function(guest_id, limit = 10) {
-// let queryString = `
-// SELECT * FROM properties 
-// WHERE guest_id $1
-// LIMIT $2;
-// `
+//   return getAllProperties(null, 2);
+// }
+
+const getAllReservations = function(guest_id, limit = 10) {
+let queryString = `
+SELECT reservations.*, properties.*, AVG(rating) AS average_rating 
+FROM reservations 
+JOIN properties ON property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = $1 AND reservations.start_date < now()
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT $2;
+`;
 // let values = [guest_id, limit]
-// return pool
-//   .query(queryString, [values])
-//   .then((result) => result.rows)
-//   .catch((err) => err.message);
-// };
+return pool
+  .query(queryString, [guest_id, limit])
+  .then((result) => result.rows)
+  .catch((err) => err.message);
+};
+
 
 exports.getAllReservations = getAllReservations;
 
